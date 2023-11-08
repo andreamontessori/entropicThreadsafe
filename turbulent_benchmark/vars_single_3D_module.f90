b16855aeddac9c7766b7e3a4e36d01dbc73e5350
andreamontessori <and.montessori@gmail.com>
2023-11-08 13:48:35 +0100

    recursive regularized turbulent channel flow benchmark folder

---
+++ turbulent_benchmark/vars_single_3D_module.f90
@@ -0,0 +1,47 @@
+module vars
+
+    implicit none
+    integer, parameter :: db=4 !kind(1.0)
+    integer :: i,j,k,ll
+    integer :: nx,ny,nz,step,stamp,stamp2D,nlinks,nsteps,ngpus
+    integer :: istat,iframe,iframe2D
+    
+    logical :: lprint,lvtk,lasync,lpbc
+    
+    real(kind=db),parameter :: pi_greek=3.14159265359793234626433
+    
+    real(kind=db)  :: ts1,ts2,p0,p1,p2,p3,p1dcssq,p2dcssq,p3dcssq
+    real(kind=db) :: visc_LB,uu,udotc,omega
+    real(kind=db) :: tau,one_ov_nu,cssq,fx,fy,fz,temp
+
+    real(kind=db) :: fneq1,feq
+    
+    integer(kind=4), allocatable,dimension(:,:,:)   :: isfluid
+    real(kind=db), allocatable, dimension(:,:,:) :: rho,u,v,w,pxx,pxy,pxz,pyy,pyz,pzz
+    real(kind=db), allocatable, dimension(:,:,:,:) :: f
+    real(kind=db) :: mymemory,totmemory
+
+    !****************************print vars**************************************!
+    
+    integer, parameter :: mxln=120
+    character(len=8), allocatable, dimension(:) :: namevarvtk
+    character(len=500), allocatable, dimension(:) :: headervtk
+    character(len=30), allocatable, dimension(:) :: footervtk
+    integer, allocatable, dimension(:) :: ndimvtk
+    integer, allocatable, dimension(:) :: vtkoffset
+    integer, allocatable, dimension(:) :: ndatavtk
+    integer, allocatable, dimension(:) :: nheadervtk
+    integer :: nfilevtk
+    integer, allocatable, dimension(:) :: varlistvtk
+    character :: delimiter
+    character(len=*), parameter :: filenamevtk='out'
+    
+    real(kind=4), allocatable, dimension(:,:,:) :: rhoprint
+    real(kind=4), allocatable, dimension(:,:,:,:) :: velprint
+    logical :: lelittle
+    character(len=mxln) :: dir_out
+    character(len=mxln) :: extentvtk
+    character(len=mxln) :: sevt1,sevt2
+    character(len=1), allocatable, dimension(:) :: head1,head2
+
+endmodule
